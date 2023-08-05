6b678cbca70de09b62edc302e6a19c76618dc1ff
alex-k22 <114990357+alex-k22@users.noreply.github.com>
2023-08-05 21:29:01 +0300

    2023-08-05

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,15 +1,22 @@
 import express from "express";
+import Joi from "joi";
 import contactsServices from "../../models/contacts.js";
-import {HttpError} from '../../helpers/index.js';
+import { HttpError } from "../../helpers/index.js";
 
 const contactsRouter = express.Router();
+
+const movieAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+});
 
 contactsRouter.get("/", async (req, res, next) => {
   try {
     const result = await contactsServices.listContacts();
     res.json(result);
   } catch (error) {
-      next(error);
+    next(error);
   }
 });
 
@@ -18,16 +25,25 @@ contactsRouter.get("/:contactId", async (req, res, next) => {
     const { contactId } = req.params;
     const result = await contactsServices.getContactById(contactId);
     if (!result) {
-      throw HttpError(404, `Contact with id=${contactId} not found`)
+      throw HttpError(404, `Contact with id=${contactId} not found`);
     }
     res.json(result);
   } catch (error) {
-      next(error);
+    next(error);
   }
 });
 
 contactsRouter.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { error } = movieAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await contactsServices.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 contactsRouter.delete("/:contactId", async (req, res, next) => {
@@ -35,7 +51,15 @@ contactsRouter.delete("/:contactId", async (req, res, next) => {
 });
 
 contactsRouter.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { error } = movieAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    console.log(req.params);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default contactsRouter;
