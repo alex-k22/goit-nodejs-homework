1aabc3efc28e695f51b360862dcbfbcba0f819df
alex-k22 <alexey.kovalov@gmail.com>
2023-08-12 12:54:01 +0000

    3rd hw made

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -1,32 +1,32 @@
 
-import contactsServices from "../models/contacts.js";
+import Contact from "../models/contact.js";
 import { HttpError } from "../helpers/index.js";
 import {ctrlWrapper} from "../decorators/index.js";
 
 
 
 const getAll = async (req, res, next) => {
-    const result = await contactsServices.listContacts();
+    const result = await Contact.find();
     res.json(result);
 };
 
-const getById = async (req, res, next) => {
+const getById = async (req, res) => {
     const { contactId } = req.params;
-    const result = await contactsServices.getContactById(contactId);
+    const result = await Contact.findById(contactId);
     if (!result) {
       throw HttpError(404, `Not found`);
     }
     res.json(result);
 };
 
-const add = async (req, res, next) => {
-    const result = await contactsServices.addContact(req.body);
+const add = async (req, res) => {
+    const result = await Contact.create(req.body);
     res.status(201).json(result);
 };
 
 const removeById = async (req, res, next) => {
     const { contactId } = req.params;
-    const result = await contactsServices.removeContact(contactId);
+    const result = await Contact.findByIdAndRemove(contactId);
     if (!result) {
       throw HttpError(404);
     }
@@ -35,9 +35,10 @@ const removeById = async (req, res, next) => {
 
 const updateById = async (req, res, next) => {
     const { contactId } = req.params;
-    const result = await contactsServices.updateContactById(
+    const result = await Contact.findByIdAndUpdate(
       contactId,
-      req.body
+      req.body,
+      {new: true}
     );
     if (!result) {
       throw HttpError(404, `Contact with id=${contactId} not found`);
@@ -45,10 +46,24 @@ const updateById = async (req, res, next) => {
     res.json(result);
 };
 
+const updateStatusContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(
+    contactId,
+    req.body,
+    {new: true}
+  );
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
 export default {
     getAll: ctrlWrapper(getAll),
     getById: ctrlWrapper(getById),
     add: ctrlWrapper(add),
     removeById: ctrlWrapper(removeById),
-    updateById: ctrlWrapper(updateById)
+    updateById: ctrlWrapper(updateById),
+    updateStatusContact: ctrlWrapper(updateStatusContact)
 }
--- server.js
+++ server.js
@@ -4,14 +4,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const {DB_HOST, PORT} = process.env;
+const { DB_HOST, PORT } = process.env;
 
-mongoose.connect(DB_HOST)
-.then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running. Use our API on port: ${PORT}`)
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+      console.log("Database connection successful");
+    });
   })
-})
-.catch(error => console.log(error.message))
-
-
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
